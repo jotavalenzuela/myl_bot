41f0dbd0d4e83b01c55d62621199147a83f9748f
jota <jota.valenzuela.roman@gmail.com>
2020-03-26 23:37:18 -0300

    fix

--- funciones.py
+++ funciones.py
@@ -50,7 +50,7 @@ def armar_mazo(message, bot):
         bot.reply_to(message, "llevas " + str(mazo['cantidad'].sum()) + " cartas... recuerda son 49 (sin contar oro inicial)")
         #print de pregunta de cartas
         forzar_respuesta = types.ForceReply(selective=False)
-        bot.send_message(chat_id, "favor ingresa el nombre de la carta que quieres agregar al mazo: ",reply_markup=forzar_respuesta)
+        bot.send_message(message.chat.id, "favor ingresa el nombre de la carta que quieres agregar al mazo: ",reply_markup=forzar_respuesta)
         #print de cantidad de cartas
         markup = types.ReplyKeyboardMarkup(row_width=2)
         itembtn1 = types.KeyboardButton('0')
@@ -58,7 +58,7 @@ def armar_mazo(message, bot):
         itembtn3 = types.KeyboardButton('2')
         itembtn4 = types.KeyboardButton('3')
         markup.add(itembtn1, itembtn2, itembtn3)
-        tb.send_message(chat_id, "cuantas copias de [insertar nombre de carta] (0 para arrepentirse):", reply_markup=markup)
+        tb.send_message(message.chat.id, "cuantas copias de [insertar nombre de carta] (0 para arrepentirse):", reply_markup=markup)
         pass
     print(mazo)
     pass
